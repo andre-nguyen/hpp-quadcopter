198b43422aae87b5c3aac1aa9e9f03acf40bade8
Joseph Mirabel <jmirabel@laas.fr>
2016-11-04 15:52:14 +0100

    Update script

--- script/quadcopter.py
+++ script/quadcopter.py
@@ -1,11 +1,11 @@
 from hpp.quadcopter import Robot
 
 robot = Robot("quad")
+robot.setJointBounds ("base_joint_xyz", [-5, 16, -4.5, 4.5, 0, 9])
+# Extra config space contains: velocity, acceleration and jerk of the center of mass of the quadrotor.
 robot.client.robot.setDimensionExtraConfigSpace(9)
-# robot.client.robot.setDimensionExtraConfigSpace(6)
-robot.setJointBounds ("base_joint_xyz", [-5, 16, -4.5, 4.5, 0, 0.6])
-# robot.client.robot.setExtraConfigSpaceBounds([-1,1,-1,1,-1,1,-1,1,-1,1,-1,1])
-robot.client.robot.setExtraConfigSpaceBounds([-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1])
+# Bounds on acceleration and jerk defined how aggressive the motion will be.
+robot.client.robot.setExtraConfigSpaceBounds([-2,2,-2,2,-2,2,-3,3,-3,3,-3,3,-1,1,-1,1,-1,1])
 
 from hpp.corbaserver import ProblemSolver
 ps = ProblemSolver (robot)
@@ -31,19 +31,15 @@ vf(q)
 #~ ps.loadObstacleFromUrdf ("iai_maps", "kitchen_area", "")
 
 ps.setInitialConfig (q_init)
-# ps.addGoalConfig (q_goal)
-ps.addGoalConfig (q)
-ps.selectPathValidation ("Discretized", 0.02)
-ps.selectSteeringMethod      ("Quadcopter")
-ps.client.problem.selectConFigurationShooter("Quadcopter")
+ps.addGoalConfig (q_goal)
+# ps.addGoalConfig (q)
 
 # ps.addPathOptimizer ("RandomShortcut")
 
-# t = ps.solve ()
-# print ("solving time", t)
+t = ps.solve ()
+print ("solving time", t)
 
 
 # gui = vf.createViewer()
-# gui.client.gui.setWireFrameMode("scene", "WIREFRAME")
+# gui.client.gui.setWireFrameMode("scene/base_link_1", "WIREFRAME")
 # pp = PathPlayer (robot.client, gui)
-
